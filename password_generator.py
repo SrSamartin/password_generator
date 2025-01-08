2ca1e2ea4a66c503b2811a0e256801aaf95836a9
Anderson Samartin <andersonsamartin@protonmail.com>
2025-01-08 10:18:25 -0300

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,6 +3,7 @@
 import random
 import string
 
+# Função que define as variáveis, englobando 3 tipos diferentes de caracteres em uma única variável denominada "options" (Opções) e definindo a variável "password_user" como uma string em branco.
 def password_generator(len_pass = 8):
     ascii_options = string.ascii_letters
     number_options = string.digits
@@ -11,6 +12,7 @@ def password_generator(len_pass = 8):
 
     password_user = ""
 
+# Loop que, dentro do escopo passado pelo usuário, define aleatóriamente cada caracter da senha.
     for i in range(0, len_pass):
         digit = random.choice(options)
         password_user = password_user + digit
@@ -25,5 +27,6 @@ else:
     print("Entrada inválida!")
     quit()
 
+# Após as verificações, é retornada a senha completa ao usuário.
 response = password_generator(len_pass = choice_user)
 print(f'Senha gerada:\n{response}')
